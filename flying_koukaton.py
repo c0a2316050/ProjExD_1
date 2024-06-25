b2f696da8de5df2ab919bf7744c7b066753bd33a
akiguru <c0a23160@edu.teu.ac.jp>
2024-06-25 15:12:02 +0900

    演習

---
+++ flying_koukaton.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    bg_img2 = pg.transform.flip(bg_img,True, False)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (300,200)
+    tmr = 0
+
+    x1 = 0
+    y1 = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        x1 = 0
+        y1 = 0
+        x = tmr%3200
+        # kk_rct.move_ip(-1,0)
+        screen.blit(bg_img,[-x,0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_img2, [-x+4800, 0])
+
+        
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            y1 -= 1
+        if key_lst[pg.K_DOWN]:
+            y1 +=1
+        if key_lst[pg.K_LEFT]:
+            x1 -=1
+        if key_lst[pg.K_RIGHT]:
+            x1 +=2
+
+        
+        screen.blit(kk_img, kk_rct)
+        x1 -=1
+
+        kk_rct.move_ip(x1,y1)
+        pg.display.update()
+        
+        tmr += 1 
+        x += 1   
+        clock.tick(200)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
